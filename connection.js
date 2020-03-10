fde12ebe45210a4f42a9625ac72daba7cdf75e75
TaylonGameta <taylon.gameta@outlook.com.br>
2020-03-10 13:06:53 -0300

    make it better

--- connection.js
+++ connection.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 const credentials = JSON.parse(require('./config'))
 
 const connection = mysql.createPool({
-    host     : credentials.host,
-    user     : credentials.user,
-    password : credentials.password,
-    database : credentials.database
+    host     : "us-cdbr-iron-east-04.cleardb.net",
+    user     : "bbca6bae0e267a",
+    password : "df1ff82b",
+    database : "heroku_36563587299327d"
 })
 
 
