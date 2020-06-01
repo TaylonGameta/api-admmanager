7db193206135717bcc8b77a857ec7eeac22a329a
TaylonGameta <taylon.gameta@outlook.com.br>
2020-06-01 15:33:19 -0300

    balance sheet endpoints

--- routes.js
+++ routes.js
@@ -2,25 +2,48 @@ const express = require('express')
 const router = express.Router()
 
 const user = require('./controllers/user')
-const bp = require('./controllers/bp')
-const aux = require('./controllers/auxiliar')
 
+const bp = require('./controllers/bp')
+const dre = require('./controllers/dre')
+
+const {now} = require('./controllers/auxiliar')
+
+
+/*
+**** USER ROUTES
+*/
 router.post('/user/register', user.register)
 
 router.post('/user/login', user.login)
 
 router.post('/user/auth', user.auth)
 
-router.get('/', user.auth, (req, res) => res.send({msg: "Taylon eh pica"}))
+router.get('/', user.auth, (req, res) => res.send({msg: "authenticated"}))
+
+/*
+**** BALANCO PATRIMONIAL ROUTES
+*/
 
 router.post('/bp/add', user.auth, bp.add)
 
+router.post('/bp/item', user.auth, bp.listBycode)
+
+router.post('/bp/delete', user.auth, bp.deleteBycode)
+
 router.get('/bp/listall', user.auth, bp.listAll)
 
-router.get('/bp/listbytype', user.auth, bp.listByType)
+router.post('/bp/listbytype', user.auth, bp.listByType)
 
-router.post('/bp/updatebpvalue', user.auth, bp.updateBpValue)
+router.post('/bp/update', user.auth, bp.update)
 
-router.get('/auxiliar/date', auxiliar.now)
+router.get('/auxiliar/date', now)
+
+
+/*
+*****DRE
+*/
+
+router.post('/dre/save', user.auth, dre.save)
+router.post('/dre/submit', user.auth, dre.submit)
 
 module.exports = router
