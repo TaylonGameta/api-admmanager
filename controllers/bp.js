33159d3a68e6188f392f68aceb757cad24acd440
TaylonGameta <taylon.gameta@outlook.com.br>
2020-06-01 15:31:38 -0300

    balance sheet functions

--- controllers/bp.js
+++ controllers/bp.js
@@ -14,7 +14,11 @@ bp = {
         const sql = `SELECT * FROM b_patrimonial_item WHERE user_id = ?`
         connection.query(sql, [id], (err, result, field)=>{
             if(err) res.send("something went wrong")
-            if(result) res.send({result})
+            if(result){
+                const total = result
+                const totalValue = total.reduce((a, v)=> a + v.b_value, 0)
+                res.send({result, totalValue})
+            }
         })
     },
     listByType : (req, res)=>{
@@ -22,13 +26,33 @@ bp = {
         const sql = `SELECT * FROM b_patrimonial_item WHERE user_id = ? AND b_type = ?`
         connection.query(sql, [id, type], (err, result, field)=>{
             if(err) res.send("something went wrong")
+            if(result){
+                const total = result
+                const totalValue = total.reduce((a, v) => a + v.b_value, 0)
+                res.send({result, totalValue})
+            }
+        })
+    },
+    update: (req, res)=>{
+        const {id, value, cod, type, name} = req.body
+        const sql = `UPDATE b_patrimonial_item SET b_value = ?, b_name = ?, b_type = ? WHERE user_id = ? AND id = ?`
+        connection.query(sql,[value, name, type, id, cod], (err, result, field)=>{
+            if(err) res.send("something went wrong")
             if(result) res.send({result})
         })
     },
-    updateBpValue: (req, res)=>{
-        const {id, value, cod} = req.body
-        const sql = `UPDATE b_patrimonial_item SET b_value = ? WHERE user_id = ? AND id = ?`
-        connection.query(sql,[value,id, cod], (err, result, field)=>{
+    listBycode: (req, res)=>{
+        const {id, cod} = req.body
+        const sql = `SELECT * FROM b_patrimonial_item WHERE user_id = ? AND id= ?`
+        connection.query(sql, [id, cod], (err, result, field)=>{
+            if(err) res.send("something went wrong")
+            if(result) res.send({result})
+        })
+    },
+    deleteBycode: (req, res)=>{
+        const {id, cod} = req.body
+        const sql = `DELETE FROM b_patrimonial_item WHERE user_id = ? AND id = ?`
+        connection.query(sql, [id, cod], (err, result, field)=>{
             if(err) res.send("something went wrong")
             if(result) res.send({result})
         })
