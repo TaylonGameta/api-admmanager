cfe6d80de8b7c85491dcd3995181a08ce3aaba97
TaylonGameta <taylon.gameta@outlook.com.br>
2020-06-04 20:08:46 -0300

    indices endpoint

--- routes.js
+++ routes.js
@@ -5,8 +5,10 @@ const user = require('./controllers/user')
 
 const bp = require('./controllers/bp')
 const dre = require('./controllers/dre')
-
+const indices = require('./controllers/indices')
 const {now} = require('./controllers/auxiliar')
+
+
 
 
 /*
@@ -40,10 +42,25 @@ router.get('/auxiliar/date', now)
 
 
 /*
-*****DRE
+***** DRE
 */
 
 router.post('/dre/save', user.auth, dre.save)
 router.post('/dre/submit', user.auth, dre.submit)
+router.post('/dre/list', user.auth, dre.list)
 
+/*
+*** INDICES
+*/
+
+router.post('/indices/get', user.auth, indices.get)
+router.post('/indices/ilc', user.auth, indices.ilc)
+router.post('/indices/ils', user.auth, indices.ils)
+router.post('/indices/ge', user.auth, indices.ge)
+router.post('/indices/ime', user.auth, indices.ime)
+router.post('/indices/pmr', user.auth, indices.pmr)
+router.post('/indices/ieg', user.auth, indices.ieg)
+router.post('/indices/mlb', user.auth, indices.mlb)
+router.post('/indices/mlo', user.auth, indices.mlo)
+router.post('/indices/mll', user.auth, indices.mll)
 module.exports = router
